c76e5c36f28de671f244a2180bd49598fffb3d57
SuperBin <404292171@qq.com>
2022-08-29 16:07:23 +0800

    运行demo

---
+++ example/android/app/src/main/java/com/jiguang/jpushexample/MyApplication.java
@@ -0,0 +1,9 @@
+package com.jiguang.jpushexample;
+
+import io.flutter.app.FlutterApplication;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MyApplication extends FlutterApplication {
+}
